6b2e512ce32dc0efae3f020b6426f412bde84533
Jamie Pace <jamiejpace@gmail.com>
2021-10-12 16:17:19 -0600

    Change password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-  
+
   validates :email, uniqueness: true, presence: true
+  
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,6 @@ RSpec.describe User do
 
   describe "validations" do
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:password) }
   end
 end
