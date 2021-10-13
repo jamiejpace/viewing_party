24777e14ba45b7cdd57a0ad78f782893a5df929f
danembb <dbrophy720@gmail.com>
2021-10-13 15:22:13 -0600

    Create bootstrap functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
 
   def login
     user = User.find_by(email: params[:email].downcase)
-    # Dane, 10/12: added user != nil &&
-    if user != nil && user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Successfully logged in!"
       redirect_to user_dashboard_path(user.id)
