a43ce5e7277b1ef9909c04db57a61c1a74c4105e
danembb <dbrophy720@gmail.com>
2021-10-13 15:30:05 -0600

    Ensure code meets rubocop convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,18 @@
 class UsersController < ApplicationController
-
   def login
     user = User.find_by(email: params[:email].downcase)
     # Dane, 10/12: added user != nil &&
-    if user != nil && user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:success] = "Successfully logged in!"
+      flash[:success] = 'Successfully logged in!'
       redirect_to user_dashboard_path(user.id)
     else
-      flash[:error] = "Unable to log in, please try again."
+      flash[:error] = 'Unable to log in, please try again.'
       redirect_to root_path
     end
   end
 
-  def new
-    
-  end
+  def new; end
 
   def dashboard
     @user = current_user
