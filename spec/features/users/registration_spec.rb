c7242c08f19740f1b22e8e2275543f2b22a7b6db
Jamie Pace <jamiejpace@gmail.com>
2021-10-13 17:17:46 -0600

    Add form to registration page, test happy path

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -10,13 +10,26 @@ require 'rails_helper'
 RSpec.describe 'user registration page' do
   before :each do
     visit registration_path
+    user1 = User.create!(email: "odell@gmail.com", password_digest: "1b3kdf9874dgtfs")
   end
 
   it 'can display registration form' do
-    save_and_open_page
     expect(page).to have_content("Email:")
     expect(page).to have_content("Password:")
     expect(page).to have_content("Password Confirmation:")
     expect(page).to have_content("Register")
+  end
+
+  it 'can register given credentials' do
+    fill_in :email, with: "mose@gmail.com"
+    fill_in :password, with: "snacksplease"
+    fill_in :password_confirmation, with: "snacksplease"
+
+    click_on "Register"
+
+    user2 = User.last
+
+    expect(current_path).to eq(user_dashboard_path(user2.id))
+    expect(page).to have_content("Welcome #{user2.email}!")
   end
 end
