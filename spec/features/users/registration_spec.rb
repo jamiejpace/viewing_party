b2109d70b10ff29d43a772e5bf0e3bd888965c9a
Jamie Pace <jamiejpace@gmail.com>
2021-10-13 18:19:52 -0600

    Refactor spec formatting

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
-# When a user visits the '/registration' path they should see a form to register.
-# The form should include:
-#
-#  Email
-#  Password
-#  Password Confirmation
-#  Register Button
-# Once the user registers they should be logged in and redirected to the dashboard page
+
 RSpec.describe 'user registration page' do
   before :each do
     visit registration_path
