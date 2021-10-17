b25f1f45accfa29da154d84a383be8d9500c8d46
Jamie Pace <jamiejpace@gmail.com>
2021-10-16 19:32:07 -0600

    Refactor movies controller logic, change view for sad path, and add vcr to movie discover spec

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,14 +2,16 @@ class MoviesController < ApplicationController
   def discover; end
 
   def index
-    @movies = MovieFacade.top_40_movies
     if params[:movie_search] && !params[:movie_search].empty?
-      @search_results = MovieFacade.movie_search(params[:movie_search])
-    elsif params[:movie_search] && params[:movie_search].empty?
-      flash.now[:error] = "Please enter something. Anything. 😕"
+      @movies = MovieFacade.movie_search(params[:movie_search])
     else
-      flash.now[:error] = "No Search Results Found 😿"
-      redirect_to movies_path
+      @movies = MovieFacade.top_40_movies
     end
+    # @movies = MovieFacade.top_40_movies
+    # if params[:movie_search] && !params[:movie_search].empty?
+    #   @search_results = MovieFacade.movie_search(params[:movie_search])
+    # elsif params[:movie_search] && params[:movie_search].empty?
+    #   flash.now[:error] = "Please enter something. Anything. 😕"
+    # end
   end
 end
