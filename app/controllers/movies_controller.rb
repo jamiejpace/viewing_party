6e778cadc6681cbd75cfb8cb169692ea80f55c9b
Jamie Pace <jamiejpace@gmail.com>
2021-10-16 19:38:06 -0600

    Delete mini test files, refactor movies controller index action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,16 +2,10 @@ class MoviesController < ApplicationController
   def discover; end
 
   def index
-    if params[:movie_search] && !params[:movie_search].empty?
+    if params[:movie_search]
       @movies = MovieFacade.movie_search(params[:movie_search])
     else
       @movies = MovieFacade.top_40_movies
     end
-    # @movies = MovieFacade.top_40_movies
-    # if params[:movie_search] && !params[:movie_search].empty?
-    #   @search_results = MovieFacade.movie_search(params[:movie_search])
-    # elsif params[:movie_search] && params[:movie_search].empty?
-    #   flash.now[:error] = "Please enter something. Anything. 😕"
-    # end
   end
 end
