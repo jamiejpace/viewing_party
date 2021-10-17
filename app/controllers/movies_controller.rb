589d7ea768116bd53e98b14b1408393e14e2058b
Jamie Pace <jamiejpace@gmail.com>
2021-10-16 21:57:26 -0600

    Rubocop corrections to movies controller and facade

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,10 +2,10 @@ class MoviesController < ApplicationController
   def discover; end
 
   def index
-    if params[:movie_search]
-      @movies = MovieFacade.movie_search(params[:movie_search])
-    else
-      @movies = MovieFacade.top_40_movies
-    end
+    @movies = if params[:movie_search]
+                MovieFacade.movie_search(params[:movie_search])
+              else
+                MovieFacade.top_40_movies
+              end
   end
 end
--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -2,14 +2,14 @@ class MovieFacade
   class << self
     def top_40_movies
       data = MovieService.top_movies(1).concat(MovieService.top_movies(2))
-      movies = data.map do |movie_data|
+      data.map do |movie_data|
         Movie.new(movie_data)
       end
     end
 
     def movie_search(search_param)
       data = MovieService.search_movies(search_param, 1).concat(MovieService.search_movies(search_param, 2))
-      returned_movies = data.map do |movie_data|
+      data.map do |movie_data|
         Movie.new(movie_data)
       end
     end
