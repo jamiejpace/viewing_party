a752cee61b2d54055d290d35e79eeff279e82bad
Jamie Pace <jamiejpace@gmail.com>
2021-10-17 19:01:38 -0600

    Add movie facade tests

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -27,4 +27,30 @@ RSpec.describe 'movie facade', :vcr do
     expect(movies.first.title).to be_a(String)
     expect(movies.count).to eq(40)
   end
+
+  it 'can return a movie that matches the given id' do
+    movie = MovieFacade.movie_details(11395)
+
+    expect(movie).to be_a(Movie)
+    expect(movie.title).to eq("The Santa Clause")
+    expect(movie.vote_average).to be_a(Float)
+    expect(movie.runtime).to be_an(Integer)
+    expect(movie.genre_names).to be_a(String)
+  end
+
+  it 'can return a cast of a movie that matches the given id' do
+    movie = MovieFacade.movie_cast(10680)
+
+    expect(movie).to be_an(Array)
+    expect(movie.first.name).to eq("Emilio Estevez")
+    expect(movie.first.role).to eq("Gordon Bombay")
+  end
+
+  it 'can return reviews of a movie that matches the given id' do
+    movie = MovieFacade.movie_reviews(2493)
+
+    expect(movie).to be_an(Array)
+    expect(movie.first.author).to be_a(String)
+    expect(movie.first.content).to be_a(String)
+  end
 end
